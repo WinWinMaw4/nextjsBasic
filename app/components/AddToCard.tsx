5d810b30844ecb9109f1f5b6e229a1a72257ce3c
WinWinMaw4 <mawinwinmaw4@gmail.com>
2024-02-11 00:02:38 +0630

    daisy ui

--- app/components/AddToCard.tsx
+++ app/components/AddToCard.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 const AddToCard = () => {
   return (
     <div>
-        <button onClick={()=>{console.log("Click")}}>Add to card</button>
+        <button className='btn btn-primary' onClick={()=>{console.log("Click")}}>Add to card</button>
     </div>
   )
 }
--- app/users/page.tsx
+++ app/users/page.tsx
@@ -2,7 +2,8 @@ import React from 'react'
 
 interface User {
   id:number;
-  name:string
+  name:string;
+  email:string;
 }
 const UserPage = async() => {
   const res = await fetch("https://jsonplaceholder.typicode.com/users",{cache: 'no-store'});
@@ -10,11 +11,21 @@ const UserPage = async() => {
   return (
 <div>
   <h1>Users</h1>
-  {/* if server side static rendering is datetime no change, if dynamic rendering is datetime will update every second(request every time), {cache: 'no-store'} */}
-    <p>{new Date().toLocaleTimeString()}</p>
-  <ul>
-    {users.map(user=><li key={user.id}>{user.name}</li>)}
-  </ul>
+{/* if server side static rendering is datetime no change, if dynamic rendering is datetime will update every second(request every time), {cache: 'no-store'} */}
+  <table className='table table-bordered'>
+    <thead>
+      <tr>
+        <th>Name</th>
+        <th>Email</th>
+      </tr>
+    </thead>
+    <tbody>
+    {users.map(user=><tr key={user.id}>
+      <td>{user.name}</td>
+      <td>{user.email}</td>
+    </tr>)}
+    </tbody>
+  </table>
 </div>
   )
 }
