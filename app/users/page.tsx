16de8a4d82c560cc6444e13731f59a55abdff116
WinWinMaw4 <mawinwinmaw4@gmail.com>
2024-02-10 23:19:27 +0630

    Static and Dynamic Rendering

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -5,11 +5,13 @@ interface User {
   name:string
 }
 const UserPage = async() => {
-  const res = await fetch("https://jsonplaceholder.typicode.com/users",{next:{revalidate: 10}});
+  const res = await fetch("https://jsonplaceholder.typicode.com/users",{cache: 'no-store'});
   const users: User[] = await res.json();
   return (
 <div>
   <h1>Users</h1>
+  {/* if server side static rendering is datetime no change, if dynamic rendering is datetime will update every second(request every time), {cache: 'no-store'} */}
+    <p>{new Date().toLocaleTimeString()}</p>
   <ul>
     {users.map(user=><li key={user.id}>{user.name}</li>)}
   </ul>
